f2bd1702006d0fd3905742a27e9d1e6d4ca9c320
Lenovo <tomerovadya04@gmail.com>
2023-11-24 21:15:20 +0200

    Big cube

--- Cube.py
+++ Cube.py
@@ -1,4 +1,5 @@
 class Cube:
+    # try to do the arr like this cube_array = [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]] it will be easier to do class faces
     def __init__(self, cubearr):
         self._cubearr = cubearr
     def cubearr(self, new_cubearr):
--- faces.py
+++ faces.py
@@ -1,8 +1,13 @@
 class faces (Cube):
 
-    def __init__(self,  age):
-        self._name = name
-        self._age = age
+    def __init__(self, cubearr):
+        super().__init__(cubearr)
+        self._L = cubearr[0:5]
+        self._R = cubearr[6:11]
+        self._U = cubearr[12:17]
+        self._D = cubearr[18:23]
+        self._B = cubearr[24:29]
+        self._F= cubearr[30:35]
 
     @property
     def name(self):
