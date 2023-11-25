4cf6c82899131ecae8057de9007687a5c1fceea0
orimood <orimood@gmail.com>
2023-11-25 13:47:50 +0200

    works

--- Cube.py
+++ Cube.py
@@ -1,8 +1,8 @@
 class Cube:
     def __init__(self, cubetext):
-        self.cubearr = []
-        for i in range(0, len(cubetext), 6):
-            self.cubearr.append(cubetext[i:i+6])
+        self.cubearr = []  # Initialize as an empty list
+        for i in range(0, 6):
+            self.cubearr.append(cubetext[i * 9:(i + 1) * 9])
 
     def get_cubearr(self):
         return self.cubearr
@@ -15,6 +15,5 @@ class Cube:
 
 text = "w r g g r g o w o r r b y o r r o b o r r b y w w y g w o b b w w g g y w g g y b w o o y r b y y g o b b y"
 stripped_text = "".join(text.split())
-print(stripped_text)
 my_cube = Cube(stripped_text)
 my_cube.print_cube()
