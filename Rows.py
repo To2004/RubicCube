9e720e23ae880d99021caded6f0a87dd3dc9dda7
orimood <orimood@gmail.com>
2023-11-25 13:36:27 +0200

    Rows fix

--- Rows.py
+++ Rows.py
@@ -5,9 +5,9 @@ class Rows (Cube):
         super().__init__(cubearr)
 
         # Assuming cubearr is a 3D array, you might want to initialize faces based on the structure
-        self._L = cubearr[0]
-        self._R = cubearr[1]
-        self._U = cubearr[2]
-        self._D = cubearr[3]
+        self._U = cubearr[0]
+        self._L = cubearr[1]
+        self._F = cubearr[2]
+        self._R = cubearr[3]
         self._B = cubearr[4]
-        self._F = cubearr[5]
+        self._D = cubearr[5]
