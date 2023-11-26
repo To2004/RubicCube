9e51bf240bcc5ac4271b99c547529c7fbc9022b5
Lenovo <tomerovadya04@gmail.com>
2023-11-26 18:26:55 +0200

    trying to do rotation F

--- Action.py
+++ Action.py
@@ -9,6 +9,18 @@ class Action (Faces,Algoritem):
     def F (self):
         # Add any validation logic if needed
         custom_cube=Algoritem.transform_cube_to_custom_shape(self.cubearr())
+        copycubearr=self.cubearr()
+        custom_cube[1][0], custom_cube[2][0], custom_cube[0][2], custom_cube[3][2] = (
+            custom_cube[2][0], custom_cube[0][2], custom_cube[3][2], custom_cube[1][0]
+        )
+
+        copyrow=custom_cube[5][0]
+        custom_cube[5][2]=copycubearr[5][0]
+        copycubearr[5][2]= custom_cube[5][2]
+        custom_cube[5][0]=copycubearr[5][2]
+        copycubearr[5][0]=  copyrow
+
+
 
 
     def R(self):
--- Algoritem.py
+++ Algoritem.py
@@ -1,7 +1,17 @@
 class Algoritem ():
- def transform_cube_to_custom_shape(cube_arr):
-    return [
-        [
-            [cube_arr[j][b][i] for b in range(3)] for i in range(3)
-        ] for j in range(6)
-    ]
+    def transform_cube_to_custom_shape(cube_arr):
+        """
+        Transforms a 3x3 Rubik's Cube into a custom shape.
+
+        Parameters:
+        - cube_arr (list): A 3D list representing the initial Rubik's Cube.
+
+        Returns:
+        - list: A 3D list representing the transformed Rubik's Cube with the desired shape(up to down).
+        """
+
+        return [
+            [
+                [cube_arr[j][b][i] for b in range(3)] for i in range(3)
+            ] for j in range(6)
+        ]
